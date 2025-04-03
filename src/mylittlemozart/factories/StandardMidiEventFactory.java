8ef995c7ab2e33b0dfe835a81bf3e80c36dbcb1d
Joseph France <jafcxm@umsystem.edu>
2025-04-03 15:59:08 -0500

    Defined the methods in each of the different factories that were defined in the interfaces.

--- src/mylittlemozart/factories/StandardMidiEventFactory.java
+++ src/mylittlemozart/factories/StandardMidiEventFactory.java
@@ -2,19 +2,22 @@ package mylittlemozart.factories;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class StandardMidiEventFactory implements MidiEventFactory{
 
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(message, tick);
 	}
 
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		ShortMessage message = new ShortMessage();
+		message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+		return new MidiEvent(message, tick);
 	}
 
 }
